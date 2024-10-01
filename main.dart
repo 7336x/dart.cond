f92cce0c55da77be4949ef3ac4dc7a72d8df4a7a
7336x <R7664alhussaini@gmail.com>
2024-10-01 08:39:02 +0300

    dart

---
+++ main.dart
@@ -0,0 +1,35 @@
+void main(){
+  int mark  = 80 ;
+  if (mark >= 80)
+  {
+    print("A");
+  }else if (mark >= 70 && mark < 80)
+  {
+    print("B");
+  } else if (mark >= 60 && mark < 70) 
+  {
+    print("C");
+
+  } else if (mark >= 50 && mark < 60)
+  {
+    print("D");
+  }else {
+    print("F");
+  }
+int i = 5 ;
+if (i% 3== 0 && i% 5==0 )
+{
+print("fizz buz");
+
+} else if (i % 3 ==0 )
+{
+  print("fizz");
+}else if ( i% 5 ==0 )
+{
+  print("buzz");
+}else {
+  print("i");
+}
+
+
+}
